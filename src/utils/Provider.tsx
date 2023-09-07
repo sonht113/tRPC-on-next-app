3c0aafe7b518b71317b75aa4687eac89eac2452d
sonht113 <trongson18101999@gmail.com>
2023-09-07 17:41:14 +0700

    feat: set up provider project

--- src/utils/Provider.tsx
+++ src/utils/Provider.tsx
@@ -1,24 +1,49 @@
 "use client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { httpBatchLink } from "@trpc/client"
+import { getFetch, httpBatchLink, loggerLink } from "@trpc/client"
 import React, { useState } from "react"
+import superjson from "superjson"
+import { NextUIProvider } from "@nextui-org/react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
 
 import { trpc } from "./trpc"
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient({}))
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: { queries: { staleTime: 3000 } }
+      })
+  )
+  const url = "http://localhost:3000/api/trpc"
+
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
+        loggerLink({
+          enabled: () => true
+        }),
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc"
+          url,
+          fetch: async (input, init?) => {
+            const fetch = getFetch()
+            return fetch(input, {
+              ...init,
+              credentials: "include"
+            })
+          }
         })
-      ]
+      ],
+      transformer: superjson
     })
   )
   return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </trpc.Provider>
+    <NextUIProvider>
+      <NextThemesProvider attribute='class' defaultTheme='dark'>
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+          <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+        </trpc.Provider>
+      </NextThemesProvider>
+    </NextUIProvider>
   )
 }
